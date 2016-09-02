259102c97aa49de12e71e9e5c6c853c4d16b6bba
rrathbone <rachellerathbone@gmail.com>
2016-09-02 14:30:11 -0700

    lint completed js

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'ryansobol/browser',
+    'ryansobol/es5',
+    'ryansobol/materialize',
+    'ryansobol/jquery'
+  ]
+};
--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
-// set text of last cell to the price of the card clicked
-var subtotal = 0;
-var tax = 0;
-//var total = 0;
-
 $(function() {
-  // Initialize collapse button
-  $(".button-collapse").sideNav();
+  'use strict';
+  var subtotal = 0;
+  var tax = 0;
 
-  $('.card-action').on('click', function(e) {
+  $('.button-collapse').sideNav();
+
+  $('.card-action').click(function(e) {
     e.preventDefault();
+
     // create row with 3 cells
     var tr = $('<tr>');
     var td1 = $('<td>');
+
     // set text of first cell to the title of the card clicked
     td1.html($('span.card-title', e.target.parentNode.parentNode).html());
-    var td2= $('<td>');
+    var td2 = $('<td>');
     var td3 = $('<td>');
+
     // set text of last cell to the price of the card clicked
     td3.html($('p.price', e.target.parentNode.parentNode).html());
+
     // append cells to the row.
     $(tr).append(td1);
     $(tr).append(td2);
     $(tr).append(td3);
 
     // append rows to the body of the table
-    $('#order-table tbody').append(tr);//.append($td);
+    $('#order-table tbody').append(tr); // .append($td);
 
     var str = $(td3).html();
     var res = str.substring(1, str.length);
+
     subtotal += parseFloat(res);
     var newSubtotal = subtotal.toFixed(2);
+
     tax = parseFloat(res);
-    //total = parseFloat(res);
+
+    // total = parseFloat(res);
     var total = (subtotal + tax).toFixed(2);
 
     tax = (newSubtotal * 0.09).toFixed(2);
@@ -44,12 +49,12 @@ $(function() {
   });
 });
 
-
 $(function() {
+  'use strict';
   $('#details-form').on('submit', function(e) {
-    e.preventDefault();
-    // $('#order-confirmation').text('Your order has been placed successfully.');
     var $toastContent = $('<span>Your order was placed successfully.</span>');
+
     Materialize.toast($toastContent, 5000);
+    e.preventDefault();
   });
 });
